0d7941cbd57fc44f6d3198bea81966c9120e5d71
tk42 <nsplat@gmail.com>
2022-02-19 22:50:14 +0900

    initial commit

---
+++ before_commit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LANG=C
+find . -type f -name "*.js" -print0 | xargs -0 sed -i '' 's@,(function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("a",{attrs:{href:t.href,target:"_blank"}},\[n("img",{attrs:{src:"https://storage.googleapis.com/studio-preview/assets/banner.png",alt:"banner",height:"18"}})\])})@@g'
